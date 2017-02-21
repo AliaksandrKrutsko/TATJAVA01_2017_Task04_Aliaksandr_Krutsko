e81a86eeb413977362a3c82134a16443f7e95b6f
Aliaksandr Krutsko <Aliaksandr_Krutsko@epam.com>
2017-02-21 13:20:03 +0300

    'update'

--- src/main/java/com/company/catalog/bean/Request.java
+++ src/main/java/com/company/catalog/bean/Request.java
@@ -27,6 +27,36 @@ public class Request implements Serializable {
     public void setSearch(String  search) {this.search = search;}
     public void setCategory(String category) {this.category = category;}
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + category.hashCode();
+        result = prime * result + command.hashCode();
+        result = prime * result + search.hashCode();
+        result = prime * result + key;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Request other = (Request) obj;
+        if (category != other.category)
+            return false;
+        if (command != other.command)
+            return false;
+        if (search != other.search)
+            return false;
+        if (key != other.key)
+            return false;
+        return true;
+    }
 
 
 
